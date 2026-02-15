d0311e54f4ea7191a969c4c95c2e69f4542ae09a
Wilenn <141501928+Wilenn@users.noreply.github.com>
2026-02-15 22:57:10 +0700

    icaa

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
-
 use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PageController;
@@ -14,6 +14,16 @@ use App\Http\Controllers\PageController;
 |--------------------------------------------------------------------------
 */
 
+// Temporary Setup Route (Auto-Seeding)
+Route::get('/setup-data', function () {
+    try {
+        Artisan::call('db:seed', ['--force' => true]);
+        return '<h1>Database Seeded Successfully!</h1><p>Data contoh (Produk, Kategori, User Admin) sudah masuk.</p><a href="/">Kembali ke Home</a>';
+    } catch (\Exception $e) {
+        return '<h1>Error Seeding Database</h1><p>' . $e->getMessage() . '</p>';
+    }
+});
+
 // Homepage
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -21,8 +31,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/produk', [ProductController::class, 'index'])->name('products.index');
 Route::get('/produk/{slug}', [ProductController::class, 'show'])->name('products.show');
 Route::get('/produk/kategori/{slug}', [ProductController::class, 'category'])->name('products.category');
-
-
 
 // Galleries
 Route::get('/galeri', [GalleryController::class, 'index'])->name('galleries.index');
